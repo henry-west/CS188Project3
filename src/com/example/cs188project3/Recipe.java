993f94bc171b4ebdc74de93650a4a04c14ad82ef
Henry West <henrywest93@outlook.com>
2014-11-28 18:04:40 -0600

    added code to get recipes from xml, not yet implemented in recipe pages

---
+++ src/com/example/cs188project3/Recipe.java
@@ -0,0 +1,49 @@
+package com.example.cs188project3;
+
+import java.util.List;
+
+public class Recipe {
+	protected String name;
+	protected String description;
+	protected String prepTime;
+	protected String servings;
+	protected List<String> ingredients;
+	protected List<String> instructions;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getPrepTime() {
+		return prepTime;
+	}
+	public void setPrepTime(String prepTime) {
+		this.prepTime = prepTime;
+	}
+	public String getServings() {
+		return servings;
+	}
+	public void setServings(String servings) {
+		this.servings = servings;
+	}
+	public List<String> getIngredients() {
+		return ingredients;
+	}
+	public void setIngredients(List<String> ingredients) {
+		this.ingredients = ingredients;
+	}
+	public List<String> getInstructions() {
+		return instructions;
+	}
+	public void setInstructions(List<String> instructions) {
+		this.instructions = instructions;
+	}
+}
---
+++ src/com/example/cs188project3/XMLProcessor.java
@@ -0,0 +1,113 @@
+package com.example.cs188project3;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import com.example.cs188project3.Recipe;
+
+public class XMLProcessor {
+
+	private static List<Recipe> recipeList = new ArrayList<Recipe>();
+	
+	public static List<Recipe> getRecipeList() {
+		return recipeList;
+	}
+
+	public static void setRecipeList(List<Recipe> l) {
+		recipeList = l;
+	}
+
+	public static void process() {
+
+
+		try {
+
+			//Get the DOM Builder Factory
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			//Get the DOM Builder
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			//Load and Parse the XML document
+			//document contains the complete XML as a Tree.
+			Document document = builder.parse(new File("recipes.xml"));
+			//Iterating through the nodes and extracting the data.
+			NodeList nodeList = document.getDocumentElement().getChildNodes();
+
+			for (int i = 0; i < nodeList.getLength(); i++) {
+
+				Node node = nodeList.item(i);
+
+				if (node instanceof Element) {
+
+					Recipe recipe = new Recipe();
+
+					//gets recipe name from ID field
+					recipe.setName(node.getAttributes().getNamedItem("id").getNodeValue());
+
+					NodeList childNodes = node.getChildNodes();
+
+					List<String> ingredients = new ArrayList<String>();
+					List<String> instructions = new ArrayList<String>();
+
+					for (int j = 0; j < childNodes.getLength(); j++) {
+
+						Node cNode = childNodes.item(j);
+						Element e;
+
+						if (cNode instanceof Element) {
+
+							String content = cNode.getLastChild().getTextContent().trim();
+
+							if (cNode.getNodeName().equals("description")) {
+								recipe.setDescription(content);
+							}
+							else if (cNode.getNodeName().equals("time")) {
+								recipe.setPrepTime(content);
+							}
+							else if (cNode.getNodeName().equals("servings")) {
+								recipe.setServings(content);
+							}
+							else if (cNode.getNodeName().equals("ingredients") || cNode.getNodeName().equals("instructions")) {
+								//populates ingredients/instructions
+								e = (Element) cNode;
+								NodeList l = e.getChildNodes();
+
+								for (int k = 0; k < l.getLength(); k++) {
+
+									Node n = l.item(k);
+
+									if (n instanceof Element) {
+
+										content = n.getLastChild().getTextContent().trim();
+
+										if (n.getNodeName().equals("ingredient")) {
+											ingredients.add(content);
+										}
+										else if (n.getNodeName().equals("step")) {
+											instructions.add(content);
+										}
+									}
+								}
+								break;
+							}
+						}
+					}
+					recipe.setIngredients(ingredients);
+					recipe.setInstructions(instructions);
+					recipeList.add(recipe);
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
